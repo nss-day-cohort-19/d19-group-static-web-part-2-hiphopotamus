ede8c71ab2cf13953a2aee93461c33a2572afddf
Joey <joeybb@MacBook-Pro.local>
2017-04-14 09:40:37 -0500

    added js file

---
+++ javascripts/main.js
@@ -0,0 +1,61 @@
+var yachts = [
+
+{
+	name:"Nauti Buoy"
+	description:"The 80 concept called for a light, fast, elegant and comfortable super yacht, taking the Cruiser / Racer concept to the next level ­ ‘Speed, seaworthiness, technology and innovation without compromise’. Nautor’s advanced ‘Regatta’ lay­up consists of S­Glass/ Aramid hybrid fibre with matched vinylester resin. The main construction parameters are weight reduction for speed, increase of strength for seaworthiness and reduction of noise and vibration for smooth and comfortable sailing. CONQUISTADOR’s condition can quite simply be summed up in one word – stunning. She has been continually maintained and cherished her throughout her life by professional crew for very discerning owners. She has also had the benefit of numerous upgrades during construction and post building. These upgrades have been made with full consultation with the factory."
+	image:
+	price:"USD 39,900,000 (EUR 37,688,000)"
+},
+{
+	name:"Aquaholic"
+	description: "The Mangusta 94' Jet Yacht is the absolute finest in Italian craftsmanship, elegance, and prestige. Dubbed the 'Ferrari of the Ocean', she is a timeless beauty equipped with state­of­the­art technology, discerning luxury and, to quote one South Florida newscaster 'she is the definition of sexy'."
+	image:
+	price:"USD 27,489,000 (EUR 25,950,000)"
+},
+{
+	name:"Piece of Ship"
+	description: "Its interior space planning is designed by experienced yachtsman and specified with a Rolls Royce propulsion system reaching top speeds of 38 knots and cruise speed of 35 knots. She is equipped with dual stabilizing gyroscopes and a telescopic and submersible swim platform. This sleek, low profile sport yacht is ideal for people who want to reach destinations fast in whisper quiet comfort."
+	image:
+	price:"USD 41,000,000 (EUR 38,727,000)"
+},
+{
+	name:"Dock Holiday"
+	description: "Designed for warm weather use and ideal for prime anchorage spots and islands proximity, the Mangusta 94' is a must have for the fun­loving owner. It is ideal for families with children or adults who entertain for pleasure and business. STELLA BIANCA boasts distinctive and unique spaces designed for the multi­use owner which can be turned from full sun to shaded areas in minutes. The Portuguese bow allows for both sunbathing and a covered dining experience for ten. The flybridge is equipped with a driving helm station that also allows for 'private' sunbathing which can also be converted into an entertaining and dining area. The aft deck converts from cocktail to dining with either shaded or open sunpads and can accommodate 15­20 people without feeling overcrowded. The extendable and submersible swim platform is an ideal gathering place for families with children and pets while the European Passarelle also doubles as a diving board."
+	image:
+	price:"USD 25,953,000 (EUR 24,500,000"
+},
+{
+	name:"In Deep Ship"
+	description: "The helm station is equipped with state­of­the­art electronics for safety, stability and maneuverability. The newest in electronic environment controls, its Sealux System, allows the entire interior/exterior functions of the yacht, including sound, lighting, blinds, HVAC, to be controlled by iPads that also connect to your iPhone to allow owner and guests to easily adjust their own preferences for environment controls and personal entertaining and listening pleasure."
+	image:
+	price:"USD 14,778,000 (EUR 13,950,000)"
+},
+{
+	name:"Ship Faced"
+	description: "Daglarca will have the ability to accommodate 8 guests in 4 luxurious cabins.This 27 meter Leopard is the best example of the very popular Italian Cruiser on the market today. ATLANTIKA is the ultimate pleasure yacht combining performance and luxury. She features the most desirable layout offered in the Leopard series, with four guest staterooms, plus galley and two cabins for three crew aft. On the foredeck are sun pads for four with Bimini top allowing sunbathing in great comfort.Leopard is recognized for superb seaworthiness and smooth handling. She has an elegant and contemporary interior with stunning details throughout. She is a MUST see vessel!!"
+	image:
+	price:"USD 7,945,000 (EUR 7,500,000)"
+},
+{
+	name:"On The Rocks"
+	description: "Mariposa 3a is a 28.95m sailing yacht delivered by Holland's Vitters yard in 1994.This superyacht has a classic design in a style reminiscent of the great racing yachts of the 1920's and 1930's. With her beautiful lines, classic sheer and deck house profile, married to a modern hull shape and highly developed keel design, the result is an extremely fast and supremely seaworthy vessel, capable of world-wide cruising in any weather."
+	image:
+	price:"USD 53,000,000 (EUR 50,061,000)"
+},
+{
+	name:"Liquid Asset"
+	description: " Dubois design, Dutch workmanship and a blonde teak interior with sculpted detailing and exquisitely designed curves throughout, ensure that Mariposa 3a stands out in both style and quality."
+	image:
+	price:"USD 84,216,000 (EUR 79,500,000)"
+},
+{
+	name:"Docked Wages"
+	description: "There is accommodation for six guests in three cabins and for up to four crew in two cabins forward. The owner's stateroom has a large queen size bed, shower room, bidet and vanity, desk, spacious hanging lockers, good stowage and all was completely refurbished in 2007. The two guest cabins both have en suite shower and head facilities, built to the same high standard."
+	image:
+	price:"USD 4,979,000 (EUR 4,700,000)"
+},
+]
+
+var container = document.getElementById("container");
+
+
